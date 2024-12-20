2ab18f722c08ea432f778d883b8f7b36a75ceef4
radriann21 <adrianrangelargu@gmail.com>
2024-12-20 17:39:22 -0400

    initialize React + TypeScript + Vite project with essential configurations and components

---
+++ src/App.tsx
@@ -0,0 +1,7 @@
+function App() {
+  return (
+    <h1 className="font-medium text-primary-custom-purple text-3xl">hello world</h1>
+  )
+}
+
+export default App
---
+++ tailwind.config.js
@@ -0,0 +1,30 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    extend: {
+      colors: {
+        primary: {
+          'custom-purple': 'hsl(259, 100%, 65%)',
+          'light-red': 'hsl(0, 100%, 67%)'
+        },
+        neutral: {
+          'off-white': 'hsl(0, 0%, 94%)',
+          'light-grey': 'hsl(0, 0%, 86%)',
+          'smokey-grey': 'hsl(0, 1%, 44%)',
+          'off-black': 'hsl(0, 0%, 8%)'
+        }
+      },
+      fontFamily: {
+        regular: ["Regular"],
+        medium: ["SemiBold"],
+        bold: ["Bold"]
+      }
+    },
+  },
+  plugins: [],
+}
+
